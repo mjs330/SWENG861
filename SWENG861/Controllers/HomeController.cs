aea0501ac492131e059d5cc4493990c2155f3802
Marc S <mjs330@users.noreply.github.com>
2020-04-18 04:17:11 -0400

    Add code comments. Add about and contact pages.

--- SWENG861/Controllers/HomeController.cs
+++ SWENG861/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,46 +10,85 @@ namespace SWENG861.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// ActionResult for the search page. Populates the page model with search results if artist or title parameters are passed
+        /// </summary>
+        /// <param name="artist"></param>
+        /// <param name="title"></param>
+        /// <returns></returns>
         public ActionResult Index(string artist = null, string title = null)
         {
-            var model = new SearchPageModel()
+            try
             {
-                ArtistResults = SpotifyController.SearchArtist(artist),
-                TrackResults  = SpotifyController.SearchTrack(title)
-            };
-            return View("/Views/Home/Index.cshtml", model);
+                var model = new SearchPageModel()
+                {
+                    ArtistResults = SpotifyController.SearchArtist(artist),
+                    TrackResults = SpotifyController.SearchTrack(title)
+                };
+                return View("/Views/Home/Index.cshtml", model);
+            }
+            catch (Exception Ex)
+            {
+                // Write error to the log and return a 500 error to the browser
+                Console.WriteLine("Exception Occurred: " + Ex.Message);
+                return new HttpStatusCodeResult((int)HttpStatusCode.InternalServerError);
+            }
         }
 
-        //public ActionResult About()
-        //{
-        //    ViewBag.Message = "Your application description page.";
+        public ActionResult About()
+        {
+            return View();
+        }
 
-        //    return View();
-        //}
+        public ActionResult Contact()
+        {
+            return View();
+        }
 
-        //public ActionResult Contact()
-        //{
-        //    ViewBag.Message = "Your contact page.";
-
-        //    return View();
-        //}
-
+        /// <summary>
+        /// ActionResult for the artist details page. Populates the page model with artist details based on the Spotify artist ID that's passed.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public ActionResult Artist(string id)
         {
-            var model = new ArtistPageModel()
+            try
             {
-                ArtistDetails = SpotifyController.GetArtistDetails(id)
-            };
-            return View("/Views/Home/Artist.cshtml", model);
+                var model = new ArtistPageModel()
+                {
+                    ArtistDetails = SpotifyController.GetArtistDetails(id)
+                };
+                return View("/Views/Home/Artist.cshtml", model);
+            }
+            catch (Exception Ex)
+            {
+                // Write error to the log and return a 500 error to the browser
+                Console.WriteLine("Exception Occurred: " + Ex.Message);
+                return new HttpStatusCodeResult((int)HttpStatusCode.InternalServerError);
+            }
         }
 
+        /// <summary>
+        /// ActionResult for the track details page. Populates the page model with artist details based on the Spotify track ID that's passed.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public ActionResult Track(string id)
         {
-            var model = new TrackPageModel()
+            try
             {
-                TrackDetails = SpotifyController.GetTrackDetails(id)
-            };
-            return View("/Views/Home/Track.cshtml", model);
+                var model = new TrackPageModel()
+                {
+                    TrackDetails = SpotifyController.GetTrackDetails(id)
+                };
+                return View("/Views/Home/Track.cshtml", model);
+            }
+            catch (Exception Ex)
+            {
+                // Write error to the log and return a 500 error to the browser
+                Console.WriteLine("Exception Occurred: " + Ex.Message);
+                return new HttpStatusCodeResult((int)HttpStatusCode.InternalServerError);
+            }
         }
     }
 }
